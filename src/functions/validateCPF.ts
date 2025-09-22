af27561a1c77ab7aa7161e26d284c233303b707f
Leandro Andrekowicz <103007824+LeandroAndrekowicz@users.noreply.github.com>
2025-09-22 11:51:02 -0300

    Função para validar cpf

---
+++ src/functions/validateCPF.ts
@@ -0,0 +1,20 @@
+export function validateCPF(cpf: string): boolean {
+  cpf = cpf.replace(/\D/g, "");
+  if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
+
+  let sum: number = 0;
+  let rest: number;
+
+  for (let i = 1; i <= 9; i++) sum += parseInt(cpf[i - 1]) * (11 - i);
+  rest = (sum * 10) % 11;
+  if (rest === 10 || rest === 11) rest = 0;
+  if (rest !== parseInt(cpf[9])) return false;
+
+  sum = 0;
+  for (let i = 1; i <= 10; i++) sum += parseInt(cpf[i - 1]) * (12 - i);
+  rest = (sum * 10) % 11;
+  if (rest === 10 || rest === 11) rest = 0;
+  if (rest !== parseInt(cpf[10])) return false;
+
+  return true;
+}
