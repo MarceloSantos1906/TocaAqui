ac15bfe2d9d05250ae20d5442be102be33b507ad
Marcelo Santos <marcelo_santos1906@hotmail.com>
2025-09-27 15:54:00 -0300

    Added schedule and payment routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import RegisterPage from './pages/Register/index';
 import HomePage from './pages/Home/index';
 import ProfessorsDetails from './pages/ProfessorsDetails/index';
 import { ToastContainer } from 'react-toastify';
+import ScheduleProfessor from './pages/ScheduleProfessor/index';
+import Payment from './pages/Payment/index';
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path='/professor/:id' element={<ProfessorsDetails />} />
+        <Route path="/professor/:id" element={<ProfessorsDetails />} />
+        <Route path="/professor/:id/schedule" element={<ScheduleProfessor />} />
+        <Route path="/professor/:id/schedule/payment" element={<Payment />} />
       </Routes>
       <ToastContainer />
     </>
