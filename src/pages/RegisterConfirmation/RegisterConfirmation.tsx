7c72504ef193dca3374cdf4b6a25a056c2fef65d
Marcelo Santos <marcelo_santos1906@hotmail.com>
2025-09-28 15:19:33 -0300

    Update RegisterConfirmation.tsx

--- src/pages/RegisterConfirmation/RegisterConfirmation.tsx
+++ src/pages/RegisterConfirmation/RegisterConfirmation.tsx
@@ -1,27 +1,77 @@
-import { Link, useNavigate, type NavigateFunction } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from "@/assets/logo.png";
 import './RegisterConfirmation.css';
-import type { User } from '../../types';
-import { createFakeJWT } from '../../functions/createToken';
 import { Bounce, toast } from 'react-toastify';
 import { z } from "zod";
-import { validateCPF } from '../../functions/validateCPF';
+import { useRegister } from "../../context/RegisterContext";
 
-const registerSchema = z.object({
-	name: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
-	email: z.email("E-mail inválido"),
-	mobileNumber: z.string().min(10, "Número de telefone inválido").max(15, "Número de telefone inválido"),
-	cpf: z.string().refine(validateCPF, { message: "CPF inválido" }),
-	profilePicture: z.url("URL de imagem inválida").optional().or(z.literal("")),
-	password: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
-	repeatPassword: z.string(),
-	role: z.string(),
-}).refine((data) => data.password === data.repeatPassword, {
-	message: "As senhas não conferem",
-	path: ["repeatPassword"],
+const confirmationSchema = z.object({
+	code: z.number().min(6, "O codigo deve conter 6 caracteres").max(6, "O codigo deve conter 6 caracteres")
 });
 
 function RegisterPage() {
+	const { cpf } = useRegister();
+	const navigate = useNavigate();
+
+	const handleConfirmation: (e: React.FormEvent<HTMLFormElement>) => void = async (e) => {
+		e.preventDefault();
+
+		const form: EventTarget & HTMLFormElement = e.currentTarget;
+		const code: string = (form.querySelector('123456') as HTMLInputElement).value;
+
+		const result = confirmationSchema.safeParse({
+			code
+		});
+
+		if (!result.success) {
+			result.error.issues.forEach((issue) => {
+				callToast(issue.message, "error");
+			});
+			return;
+		}
+
+		try {
+			const response = await fetch("/api/person/activate-account", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({
+					cpf,
+					code: result.data.code,
+				}
+				),
+			});
+
+			if (!response.ok) {
+				const errorData = await response.json();
+				callToast(errorData.message || "Erro ao fazer login", "error");
+				return;
+			}
+
+			const data = await response.json();
+			localStorage.setItem("token", data.token);
+
+			callToast("Login realizado com sucesso!", "success");
+			navigate("/");
+		} catch (error) {
+			callToast("Erro de conexão com o servidor", "error");
+			console.error("Login error:", error);
+		}
+	};
+
+	const callToast = (message: string, type: "success" | "error" = "success") => {
+		toast[type](message, {
+			position: "top-right",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: false,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "dark",
+			transition: Bounce,
+		});
+	};
+
 	return (
 		<main className="main-container">
 			<section className="left-panel">
@@ -33,8 +83,9 @@ function RegisterPage() {
 				<div className="email-instruction">
 					<h2>Insira o código enviado ao seu e-mail.</h2>
 				</div>
-				<form className="confirmation-form">
+				<form className="confirmation-form" onSubmit={handleConfirmation}>
 					<input type="text" id="confirmation-code" name="confirmation-code" className="confirmation-input" placeholder="Digite o código aqui" />
+					<br />
 					<button type="submit" className="confirm-button">Confirmar</button>
 				</form>
 				<div className="back-to-login">
@@ -43,7 +94,7 @@ function RegisterPage() {
 				<footer className="footer-text">
 					<p>Todos direitos reservados Tocaqui LTDA.</p>
 				</footer>
-			</section>	
+			</section>
 		</main>
 	);
 }
